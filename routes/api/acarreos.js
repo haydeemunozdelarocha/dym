d41e4689f72c20ed74a3d208d504bd858df9276a
Haydeé Muñoz <haydeemunozdelarocha@gmail.com>
2017-06-20 06:21:23 -0600

    fixed date on imprimir recibo captura

--- routes/api/acarreos.js
+++ routes/api/acarreos.js
@@ -255,8 +255,7 @@ router.get('/totales/:obra', function(req, res, next ){
 })
 
 router.post('/recibos/resumen',isLoggedIn, function(req, res, next ){
-  var date= req.body.fechaRecibo;
-  console.log(date);
+  var date= req.body.fecha;
   var hora = moment(date).format("YYYY-MM-DD");
   var hora_recibo = moment(date).format("YYYY-MM-DD HH:mm");
   var date1 = hora + " 00:00";
